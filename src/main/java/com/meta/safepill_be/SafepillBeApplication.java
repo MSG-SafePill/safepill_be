6bc7c7c063a6f279757d710acdbae1f52285a809
ryukh1129 <ryukh1129@gmail.com>
2026-03-14 10:48:56 +0900

    chore: First commit

---
+++ src/main/java/com/meta/safepill_be/SafepillBeApplication.java
@@ -0,0 +1,13 @@
+package com.meta.safepill_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafepillBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafepillBeApplication.class, args);
+    }
+
+}
