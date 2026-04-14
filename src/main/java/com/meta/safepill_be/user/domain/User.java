453e141d40d65f9022fb27e81e5c3ddd4b07cd9c
ryukh1129 <ryukh1129@gmail.com>
2026-04-14 22:24:06 +0900

    chore: MySQL에서 PostgreSQL로 데이터베이스 환경 마이그레이션
    - build.gradle: 기존 MySQL 드라이버 의존성 제거 및 PostgreSQL JDBC 드라이버 추가
    - application.properties: DB 접속 URL, 인증 정보 및 Hibernate 방언을 PostgreSQL 환경으로 전면 수정
    - 기존 DB 데이터 전체 이관 및 시퀀스 동기화 완료
    - 인텔리제이 '명령줄 줄이기' 설정 적용을 통해 긴 클래스패스로 인한 실행 에러 방어

--- src/main/java/com/meta/safepill_be/user/domain/User.java
+++ src/main/java/com/meta/safepill_be/user/domain/User.java
@@ -16,7 +16,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User extends TimeStamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
